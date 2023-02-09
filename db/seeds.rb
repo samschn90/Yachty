b0f0e6c33dd9d40ac2be5f1711970a5e41dcad4f
anna schäffner <anna-schaeffner@gmx.de>
2023-02-09 20:11:25 +0100

    seeds für yachten und user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+puts "Destroying records"
+
+User.destroy_all
+Yacht.destroy_all
+Booking.destroy_all
+
+i = 0
+10.times do
+  puts "Creating user #{i}"
+  user = User.new(
+    email: "#{i}@gmail.com",
+    first_name: "name",
+    last_name: "lastname",
+    password: "123456"
+  )
+  user.save!
+  puts "Creating user's Yacht"
+  Yacht.create(
+    name: Faker::Music.mambo_no_5,
+    length: Faker::Number.within(range: 2.0..30.0), description: Faker::Quote.robin,
+    price: Faker::Number.within(range: 50.0..5000000.0),
+    user: user
+  ).save!
+  i += 1
+end
+
+puts "Seeding Finished"
