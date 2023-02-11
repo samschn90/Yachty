5f1e9224717eee45aa0cc0e816c4990ce4d709bc
Samuel Schneider <samuel@skschneider.ch>
2023-02-11 15:39:30 +0100

    ist egal

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -1,4 +1,10 @@
 class Yacht < ApplicationRecord
+  has_many :bookings
+
+  def available?(start_time, end_time)
+    bookings.where("start_time <= ? AND end_time >= ?", end_time, start_time).empty?
+  end
+
   belongs_to :user
   has_one_attached :photo
 end
